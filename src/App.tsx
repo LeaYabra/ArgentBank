34c79996feb0871ecd0c080f4d4284b80288fdd8
lea yabra <yabralea@gmail.com>
2023-11-12 18:12:23 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import Header from "./features/header/Header"
+import Home from "./features/home/HomePage"
+import Footer from "./features/footer/Footer"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/features/header/Header.tsx
@@ -0,0 +1,24 @@
+import logo from "../../designs/img/argentBankLogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+
+
+function Header() {
+  return (
+    <nav className="main-nav">
+      <a className="main-nav-logo" href="./index.html">
+        <img 
+          className="main-nav-logo-image"
+          src={logo}
+          alt="Argent Bank Logo"/>
+        <h1 className="sr-only">Argent Bank</h1>
+      </a>
+      <div>
+        <a className="main-nav-item" href="./sign-in.html">
+        <FontAwesomeIcon icon={faUserCircle}/>  Sign In
+        </a>
+      </div>
+    </nav>
+  );
+}
+export default Header
