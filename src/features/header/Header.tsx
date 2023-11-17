83e5d569b339c32e72c568fdc45f9ed43360f61b
luc <luc.leonardi@opsonet.com>
2023-11-17 10:23:30 +0100

    setting begin router

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,24 +1,24 @@
 import logo from "../../designs/img/argentBankLogo.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUserCircle } from "@fortawesome/free-regular-svg-icons"
-// import SignPage from '../../pages/SignPage';
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link className="main-nav-logo" to={"/"}>
         <img
           className="main-nav-logo-image"
           src={logo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="/sign-in">
+        <Link className="main-nav-item" to={"/sign-in"}>
           <FontAwesomeIcon icon={faUserCircle} />
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,27 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import App from "./App"
+import Home from "./pages/Home"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 // import SignIn from "./pages/SignPage"
 import "./index.css"
+import SignIn from "./pages/SignPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
